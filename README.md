cecfe4f17410c75df447cd5f419198945c404b86
Josh Asres <jsasres@gmail.com>
2022-12-16 15:43:17 -0600

    added script to repo and made additions to readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # weatherES
-Simple python script to get your local weather and ship it to an Elasticsearch cluster
+Simple python script to get your local weather and ship it to an Elasticsearch cluster. Uses the openweather API so you'll need to sign up for an account to generate an API key. Additionally the script is written for connecting to a Elastic Cloud deploymnet rather than a local cluster.
+
+You'll need the Elasticsearch python module as well as the pytz module for getting the local time.
---
+++ weatherES.py
@@ -0,0 +1,47 @@
+
+# Import the necessary libraries
+import requests
+from elasticsearch import Elasticsearch
+from datetime import datetime
+import pytz
+
+
+# Define the location for which to check the weather
+
+date = datetime.now(pytz.timezone('US/Central')) #Add local timezone
+lat = "XXXX"
+lon = "XXXX"
+# Use the OpenWeatherMap API (with key) to get the current weather for the specified location
+api_key = "XXXX" 
+url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=imperial&appid={api_key}"
+response = requests.get(url)
+
+# Grab the temperature, conditions, and humidity from the API response
+weather_data = response.json()
+main = weather_data['main']
+temperature = main['temp']
+conditions = weather_data['weather']
+humidity = main['humidity']
+
+#establish connection to ES cloud cluster with API key and deployment ID 
+ELASTIC_KEY = "XXXX"
+CLOUD_ID = "XXXX"
+es = Elasticsearch(
+    cloud_id=CLOUD_ID,
+    basic_auth=("elastic", ELASTIC_KEY)
+)
+
+#build json document
+doc = {
+    'temperature': temperature,
+    'conditions': conditions[0]['description'],
+    'humidity': humidity,
+    'timestamp': date,
+}
+
+payload = es.index(index="weather", document=doc) #call ES index API
+
+print(payload['result'])
+
+
+
