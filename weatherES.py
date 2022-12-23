5fe93b3115b87c8f7a0a8e77180dbc63ea8dd212
Josh Asres <jsasres@gmail.com>
2022-12-23 14:09:59 -0500

    updated script to run repeatedly for a defined amount of time instead of just once,also updated the readme to reflect this change

--- weatherES.py
+++ weatherES.py
@@ -4,24 +4,15 @@ import requests
 from elasticsearch import Elasticsearch
 from datetime import datetime
 import pytz
+import time
 
-
-# Define the location for which to check the weather
-
-date = datetime.now(pytz.timezone('US/Central')) #Add local timezone
+#latitude and longitude of the location
 lat = "XXXX"
 lon = "XXXX"
-# Use the OpenWeatherMap API (with key) to get the current weather for the specified location
+#your OpenWeatherMap API key
 api_key = "XXXX" 
 url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=imperial&appid={api_key}"
-response = requests.get(url)
 
-# Grab the temperature, conditions, and humidity from the API response
-weather_data = response.json()
-main = weather_data['main']
-temperature = main['temp']
-conditions = weather_data['weather']
-humidity = main['humidity']
 
 #establish connection to ES cloud cluster with basic auth and deployment ID 
 ELASTIC_PASS = "XXXX"
@@ -31,17 +22,35 @@ es = Elasticsearch(
     basic_auth=("elastic", ELASTIC_PASS)
 )
 
-#build json document
-doc = {
+
+run_time = int(3600) #length of time the script should run, default is one hour
+start_time = time.time()  # time we started
+while (time.time() - start_time) < run_time:
+    date = datetime.now(pytz.timezone('US/Central')) # define the timezone 
+    response = requests.get(url) # call openweather API
+
+    # Grab the temperature, conditions, and humidity from the API response
+
+    weather_data = response.json()
+    main = weather_data['main']
+    temperature = main['temp']
+    conditions = weather_data['weather']
+    humidity = main['humidity']
+
+    #build json document
+    doc = {
     'temperature': temperature,
     'conditions': conditions[0]['description'],
     'humidity': humidity,
     'timestamp': date,
-}
+    }
+    
+    payload = es.index(index="weather", document=doc) #call ES index API
+    print(payload['result'])
+    print(doc)
+    time.sleep(300) #time in between each exection, default set to 5 mins
 
-payload = es.index(index="weather", document=doc) #call ES index API
 
-print(payload['result'])
 
 
 
